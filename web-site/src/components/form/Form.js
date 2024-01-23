ecbd83e7aed25e440b9bc3cc9b5548dc30f32e19
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-01-23 20:56:02 +0500

    adapted form component for 1440px 1024px

--- web-site/src/components/form/Form.js
+++ web-site/src/components/form/Form.js
@@ -1,30 +1,33 @@
 import React from 'react'
 import classes from './form.module.scss'
 
+
+
 export default function Form(){
     return(
         <div className={classes.form}>
             <div className={classes.formContainer}>
-                <div className={classes.formBg}>
-                    {/* <div className={classes.bgText}>
+                <div className={classes.formText}>
                         <h1>Оставьте свои контакты <br /> и проектные ожидания, <br /> мы свяжемся с вами в <br /> кратчайшие сроки для <br /> обсуждения идеального  <br /> веб-проекта.</h1>
                         <p>Свяжемся с вами вкратчайшее время!</p>
-                    </div> */}
-                    {/* <form>
-                        <label className={classes.formLabel} htmlFor="">Заявка</label>
-                        <div className={classes.formContnent}>
-                            <div >
-                                <input type="name" name='name' placeholder='Имя'/>
-                                <input type="tel" name='tel' placeholder='Телефон'/>
-                                <input type="email" name='email' placeholder='E-mail'/>
-                                <textarea name="" id="" cols="30" rows="10" placeholder='Сообщение'></textarea>
-                            </div>
+                </div>
+                <form>
+                    <label className={classes.formLabel} htmlFor="">Заявка</label>
+                    <div className={classes.formContent}>
+                        <div>
+                            <input placeholder='Имя' type="name" required className={classes.formItem}/>
+                            <input placeholder='Телефон' type="tel" required className={classes.formItem}/>
+                            <input placeholder='E-mail' type="email" required className={classes.formItem}/>
+                            <textarea placeholder='Сообщение' type="text" required className={classes.formItem} id={classes.textareaItem}/>
+                        </div>
+                        <div className={classes.submitContainer}>
                             <input type="submit" name='submit' className={classes.submitBtn}/>
                         </div>
-                    </form> */}
-                </div>
+                    </div>
+                </form>
             </div>
         </div>
     )
 }
+
 
