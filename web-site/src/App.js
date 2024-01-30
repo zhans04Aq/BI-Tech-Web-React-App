0c30e34c60be0d8dd7c33591c1ff839b6f459bc3
zhans04Aq <145835709+zhans04Aq@users.noreply.github.com>
2024-01-30 15:29:57 +0500

    better icons and added whatsapp fixed icon with link

--- web-site/src/App.js
+++ web-site/src/App.js
@@ -11,9 +11,14 @@ import Review from './components/reviews/Review';
 import Footer  from './components/footer/Footer';
 
 // import components
+// import img
+import whIcon from './assets/icon/contact_wh_icon.png'
+// import img
+
 function App() {
   return (
     <div className="App">
+      <a className='whatsapp-link' href="https://wa.me/77774345295"><img className="whatsapp-icon" src={whIcon} alt="" /></a>
       <div className='Container'>
         <Header/>
         <Form/>
